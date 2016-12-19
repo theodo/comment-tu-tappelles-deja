72acefa225b1999aef1b9640236fd39353c42354
Gireg <gireg.dekerdanet@hotmail.fr>
2016-12-20 00:27:28 +0100

    add reminder at 4pm everyday

--- background/back.js
+++ background/back.js
@@ -1,4 +1,7 @@
 chrome.browserAction.onClicked.addListener(function() {
+  chrome.browserAction.setIcon({path: 'icons/ic_person_pin_circle_black_18dp_2x.png'});
+  localStorage.setItem("sessionDoneAt", new Date().getDate());
+
   var theodoTeamPage = {
     url: "http://www.theodo.fr/fr/theodo/les-theodoers"
   };
@@ -13,4 +16,15 @@ chrome.browserAction.onClicked.addListener(function() {
   };
 
   chrome.tabs.create(theodoTeamPage, injectScripts);
+});
+
+// Fire a reminder alarm everyday at 4pm
+var date = new Date();
+var startOfDay = date.getTime() - date.getSeconds() - date.getMinutes() - date.getHours();
+var alarm = chrome.alarms.create("reminder", {when: startOfDay + 16*3600*1000, periodInMinutes: 24*60});
+chrome.alarms.onAlarm.addListener(function(alarm) {
+  var sessionDoneAt = localStorage.getItem("sessionDoneAt");
+  if(typeof sessionDoneAt === "undefined" || sessionDoneAt != new Date().getDate()) {
+    chrome.browserAction.setIcon({path: 'icons/ic_person_pin_circle_black_18dp_2x-red.png'});
+  }
 });
